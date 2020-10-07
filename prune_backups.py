067f8a7f0e9eaee8b0fe86e293f1fb3743ba69d9
Spok <m.szpakowicz@mail.utoronto.ca>
2020-10-07 12:29:48 -0400

    added argument parsing

--- prune_backups.py
+++ prune_backups.py
@@ -5,12 +5,16 @@ if __name__ == "__main__":
 
     # parse arguments
     parser = argparse.ArgumentParser()
-
     parser.add_argument("-t", help="specifies the target directory from which to prune backups",
                         action='store', dest="target")
-    
     args = parser.parse_args()
     
-    # 
-    backups = Path("backups")
-    backups.mkdir(exist_ok=True)
+    # select backups folder appropriately
+    # default location of backups folder in same dir as script
+    backups = Path(__file__).resolve().parent / "backups"
+    if args.target:
+        backups = Path(args.target)
+    
+    # throw error if restoring from invalid target
+    if not backups.is_dir():
+        raise FileNotFoundError("ERROR: Target backups directory does not exist")
