df3004179b5ef79769cc361e63958771f1c20d98
Spok <m.szpakowicz@mail.utoronto.ca>
2020-10-12 14:31:15 -0400

    I'm done.. ish?

--- prune_backups.py
+++ prune_backups.py
@@ -1,5 +1,6 @@
 import argparse
 from pathlib import Path
+from datetime import datetime, timedelta
 
 if __name__ == "__main__":
 
@@ -18,3 +19,50 @@ if __name__ == "__main__":
     # throw error if restoring from invalid target
     if not backups.is_dir():
         raise FileNotFoundError("ERROR: Target backups directory does not exist")
+    
+    reversed_paths = reversed([p for p in backups.glob("*")])
+    paths_to_remove = []
+    removed_something = False
+    
+    ref_date = None
+    last_path = None
+    
+    i = 1
+    delta_max = [timedelta(0), timedelta(1), timedelta(7), timedelta(30), timedelta(365), timedelta.max]
+    delta_min = delta_max[i]
+
+    for curr_path in reversed_paths:
+
+        if last_path and str(last_path.stem)[:-15] == str(curr_path.stem)[:-15]:
+
+            dlst = str(curr_path.stem)[:-15].split("-")
+            cur_date = datetime(int(dlst[0]), int(dlst[1]), int(dlst[2]), int(dlst[3]), int([4][:2]), int(dlst[4][2:]))
+
+            timediff = ref_date - cur_date
+
+            # if greater than current max, move to next max
+            if timediff > delta_max[i]:
+                i += 1
+                delta_min = delta_max[i]
+                if removed_something:
+                    paths_to_remove.pop(-1)
+                    removed_something = False
+            # if greater than next step to max, add back the last removed item
+            elif timediff > delta_min + delta_max[i - 1]:
+                delta_min += delta_max[i - 1]
+                if removed_something:
+                    paths_to_remove.pop(-1)
+                    removed_something = False
+            # remove other paths greater than the minimum
+            elif timediff > delta_min:
+                paths_to_remove.append(last_path)
+                removed_something = True
+        else:
+            
+            removed_something = False
+
+            dlst = str(curr_path.stem)[:-15].split("-")
+            ref_date = datetime(int(dlst[0]), int(dlst[1]), int(dlst[2]), int(dlst[3]), int([4][:2]), int(dlst[4][2:]))
+            last_path = curr_path
+            
+            i = 0
